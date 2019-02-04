b46036a2aad40a25a3fe3e7d8b51135efd1d1efb
MyBucketIsFull <brax.bram@gmail.com>
2019-02-04 11:07:59 +0100

    Added error handling Stats

--- stats/views.py
+++ stats/views.py
@@ -17,21 +17,26 @@ def execute_list(request):
     else:
         get = request.GET.get('input')
 
+        output = ''
+        error = ''
         if get is not None:
             data = {'input': get}
             form = ExecuteForm(initial=data)
 
             analyzer = Analyzer()
             ints = [int(s) for s in re.findall(r'\d+', get)]
-            output = {
-                'Maximum': analyzer.calculate_maxmimum(ints),
-                'Sum': analyzer.calculate_sum(ints),
-                'Mean': analyzer.calculate_mean(ints),
-                'Variance': analyzer.calculate_variance(ints),
-                'Standard Deviation': analyzer.calculate_standard_deviation(ints)
-            }
+
+            if len(ints) > 0:
+                output = {
+                    'Maximum': analyzer.calculate_maxmimum(ints),
+                    'Sum': analyzer.calculate_sum(ints),
+                    'Mean': analyzer.calculate_mean(ints),
+                    'Variance': analyzer.calculate_variance(ints),
+                    'Standard Deviation': analyzer.calculate_standard_deviation(ints)
+                }
+            else:
+                error = 'Fill in at least one integer.'
         else:
             form = ExecuteForm()
-            output = ''
 
-        return render(request, 'stats/execute_list.html', {'form': form, 'out': output})
+        return render(request, 'stats/execute_list.html', {'form': form, 'out': output, 'error': error})
